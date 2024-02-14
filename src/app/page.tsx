2ff655eacb42517dfada155e71f9540ed0599ed2
Jacob <halter_curds.0r@icloud.com>
2024-02-14 13:56:20 -0600

    Refactor page layout to include header and section for job listings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,18 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main>{jobs?.map((job) => <JobListItem key={job.id} job={job} />)}</main>
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Developer Jobs
+        </h1>
+        <p className="text-muted-foreground">Find NextJS jobs</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs?.map((job) => <JobListItem key={job.id} job={job} />)}
+        </div>
+      </section>
+    </main>
   );
 }
