d4bdbbebe572a5374a5a50172f9feb71a0c7717e
Jacob <halter_curds.0r@icloud.com>
2024-02-14 16:21:01 -0600

    Add JobFilterSidebar component and Select component

--- src/components/JobFilterSidebar.tsx
+++ src/components/JobFilterSidebar.tsx
@@ -1,11 +1,27 @@
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import Select from "./ui/select";
+import prisma from "@/lib/prisma";
 
 async function filterJobs(formData: FormData) {
   "use server";
 }
 
-export default function JobFilterSidebar() {
+export default async function JobFilterSidebar() {
+  const distinctLocations = (await prisma?.job
+    .findMany({
+      where: {
+        approved: true,
+      },
+      select: {
+        location: true,
+      },
+      distinct: ["location"],
+    })
+    .then((locations) =>
+      locations.map(({ location }) => location).filter(Boolean),
+    )) as string[];
+
   return (
     <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
       <form action={filterJobs}>
@@ -13,6 +29,17 @@ export default function JobFilterSidebar() {
           <div className="flex flex-col gap-2">
             <Label htmlFor="q">Search</Label>
             <Input id="q" name="q" placeholder="Title, company, etc" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select className="" id="location" name="location" defaultValue="">
+              <option value="">All Locations</option>
+              {distinctLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
           </div>
         </div>
       </form>
