d79fe2ef25bb8aa561fa3713d037ead597db9de0
Jacob <halter_curds.0r@icloud.com>
2024-02-14 16:56:46 -0600

    Add Error component and use H1 component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import JobListItem from "@/components/JobListItem";
+import H1 from "@/components/ui/h1";
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
@@ -12,9 +13,7 @@ export default async function Home() {
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Developer Jobs
-        </h1>
+        <H1>Developer Jobs</H1>
         <p className="text-muted-foreground">Find NextJS jobs</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
