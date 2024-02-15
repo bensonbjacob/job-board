38b7444304ff1fabc08f64c7a915d55b8750d7c9
Jacob <halter_curds.0r@icloud.com>
2024-02-15 12:06:00 -0600

    Add new job form component

---
+++ src/app/jobs/new/NewJobForm.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import H1 from "@/components/ui/h1";
+import { CreateJobValues, createJobSchema } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import Select from "@/components/ui/select";
+import { jobTypes } from "@/lib/job-types";
+
+export default function NewJobForm() {
+  const form = useForm<CreateJobValues>({
+    resolver: zodResolver(createJobSchema),
+  });
+
+  const {
+    handleSubmit,
+    watch,
+    trigger,
+    control,
+    setValue,
+    setFocus,
+    formState: { isSubmitting },
+  } = form;
+
+  async function onSubmit(values: CreateJobValues) {
+    console.log(values);
+  }
+
+  return (
+    <main className="m-auto my-10 max-w-3xl space-y-10">
+      <div className="space-y-5 text-center">
+        <H1>Find the perfect developer</H1>
+        <p className="text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
+      </div>
+      <div className="rouded-lg space-y-6 border p-4">
+        <div>
+          <h2 className="font-semibold">Job details</h2>
+          <p className="text-muted-foreground">
+            Add job description and details
+          </p>
+        </div>
+        <Form {...form}>
+          <form
+            className="space-y-4"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="title">Job title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g. Fullstack Developer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="title">Job type</FormLabel>
+                  <FormControl>
+                    <Select {...field} defaultValue="">
+                      <option value="" hidden>
+                        Select job type
+                      </option>
+                      {jobTypes.map((jobType) => (
+                        <option key={jobType} value={jobType}>
+                          {jobType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+    </main>
+  );
+}
