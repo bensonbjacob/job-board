80da99889b6cc246181503ccef645b57ef0f8d16
Jacob <halter_curds.0r@icloud.com>
2024-02-15 08:37:37 -0600

    Add Navbar component to layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <header className="shadow-sm">
+      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
+        <Link href="/" className="flex items-center gap-3">
+          <Image src={logo} alt="Developer Jobs Logo" width={40} height={40} />
+          <span className="text-xl font-bold tracking-tight">
+            Developer Jobs
+          </span>
+        </Link>
+        <Button asChild>
+          <Link href="/jobs/new">Post a Job</Link>
+        </Button>
+      </nav>
+    </header>
+  );
+}
