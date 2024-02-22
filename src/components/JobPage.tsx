86c965a88df588687a48d877ae3450c7004cad5f
Jacob <halter_curds.0r@icloud.com>
2024-02-22 08:56:34 -0600

    Add Markdown component for rendering formatted text

--- src/components/JobPage.tsx
+++ src/components/JobPage.tsx
@@ -4,6 +4,7 @@ import { Briefcase, MapPin, Globe2, Banknote, Clock } from "lucide-react";
 import { Span } from "next/dist/trace";
 import Image from "next/image";
 import Link from "next/link";
+import Markdown from "./Markdown";
 
 interface JobPageProps {
   job: Job;
@@ -70,7 +71,7 @@ export default function JobPage({
           </div>
         </div>
       </div>
-      <div>{description}</div>
+      <div>{description && <Markdown>{description}</Markdown>}</div>
     </section>
   );
 }
